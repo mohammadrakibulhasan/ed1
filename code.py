41f8def4f7154ad3c4e94eb10e13c7d14da9da9c
MD. Rakibul Hasan <58723960+mohammadrakibulhasan@users.noreply.github.com>
2023-09-08 19:09:38 +0600

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as plt # visualizing data
+#import matplotlib.pyplot as plt # visualizing data
 import seaborn as sns
 
 # import csv file
